c31c9202683c4f884464867721ecebc5b1550e1b
adalito228 <adaltac8@gmail.com>
2024-07-30 19:36:20 +0200

    reverse proxy added

--- client/admin/front-admin/vite.config.js
+++ client/admin/front-admin/vite.config.js
@@ -1,6 +1,23 @@
-export default {
+/* export default {
   base: '/admin',
   server: {
     port: 5171
   }
 }
+*/
+const { defineConfig } = require('vite')
+
+export default defineConfig({
+  base: '/admin',
+  server: {
+    port: 5171
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    minify: 'terser',
+    rollupOptions: {
+      input: '/src/index.js'
+    }
+  }
+})
